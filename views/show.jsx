59b41d2f79c19aab53e7c400dee0b96715f932c7
Miguel C <migs.castaneda@gmail.com>
2022-07-09 12:23:07 -0400

    add edit feature

--- views/show.jsx
+++ views/show.jsx
@@ -18,6 +18,8 @@ function Show ({bread, index}) {
         <img src={bread.image} alt={bread.name}></img>
         <li><a href="/breads">Go Home</a></li>
         
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
         <form action={`/breads/${index}?_method=DELETE`} method="POST">
           <input type='submit' value="DELETE"/>
         </form>
