cda93e831e6e76927fc2ceeede878addf3f66c09
Miguel C <migs.castaneda@gmail.com>
2022-07-16 10:44:22 -0400

    delete and edit work

--- views/show.jsx
+++ views/show.jsx
@@ -18,9 +18,9 @@ function Show ({bread, index}) {
         <img src={bread.image} alt={bread.name}></img>
         <li><a href="/breads">Go Home</a></li>
         
-        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+        <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
 
-        <form action={`/breads/${index}?_method=DELETE`} method="POST">
+        <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
           <input type='submit' value="DELETE"/>
         </form>
 
